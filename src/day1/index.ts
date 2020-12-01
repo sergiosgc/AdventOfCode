154f1f0b94b285514af223b3340fe6a64c49dc0a
Sérgio Carvalho <sergio.carvalho@sooma.com>
2020-12-01 12:07:22 +0000

    Rename index variable for clarity

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -5,10 +5,10 @@ const prepareInput = (rawInput: string) => rawInput.split("\n").map( (s:string):
 const input = prepareInput(readInput())
 
 const nSumProduct = function(n: number, target: number, input: Array<number>): number {
-  for (let index: Array<number> = [ 0 ]; index.length > 0; index[index.length - 1]++) {
-    if (index[index.length - 1] == input.length) index.pop();
-    else if (index.length < n) index.push(index[index.length - 1]);
-    else if ( target == index.reduce( (acc, i) => acc + input[i], 0 )) return index.reduce( (acc, i) => acc * input[i], 1 );
+  for (let indexStack: Array<number> = [ 0 ]; indexStack.length > 0; indexStack[indexStack.length - 1]++) {
+    if (indexStack[indexStack.length - 1] == input.length) indexStack.pop();
+    else if (indexStack.length < n) indexStack.push(indexStack[indexStack.length - 1]);
+    else if ( target == indexStack.reduce( (acc, i) => acc + input[i], 0 )) return indexStack.reduce( (acc, i) => acc * input[i], 1 );
   }
   return null;
 }
