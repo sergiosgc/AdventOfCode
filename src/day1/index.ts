0421a736dbfee4019a5d06c5ff6b6e244e4944d6
Sérgio Carvalho <sergio.carvalho@sooma.com>
2020-12-01 12:04:32 +0000

    Account for no solution (outside problem scope)

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -5,11 +5,12 @@ const prepareInput = (rawInput: string) => rawInput.split("\n").map( (s:string):
 const input = prepareInput(readInput())
 
 const nSumProduct = function(n: number, target: number, input: Array<number>): number {
-  for (let index: Array<number> = [ 0 ]; true; index[index.length - 1]++) {
+  for (let index: Array<number> = [ 0 ]; index.length > 0; index[index.length - 1]++) {
     if (index[index.length - 1] == input.length) index.pop();
     else if (index.length < n) index.push(index[index.length - 1]);
     else if ( target == index.reduce( (acc, i) => acc + input[i], 0 )) return index.reduce( (acc, i) => acc * input[i], 1 );
   }
+  return null;
 }
 
 console.time("Time")
