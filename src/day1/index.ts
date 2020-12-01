f86264dbccd38637a6b969199200ad4b07427e09
Sérgio Carvalho <sergio.carvalho@sooma.com>
2020-12-01 12:00:17 +0000

    Refactor day1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,27 +1,18 @@
 import { test, readInput } from "../utils/index"
 
-const prepareInput = (rawInput: string) => rawInput.split("\n").map( (s:string):number => +s);
+const prepareInput = (rawInput: string) => rawInput.split("\n").map( (s:string):number => +s).sort( (a,b) => a-b );
 
 const input = prepareInput(readInput())
 
-const goA = (input) => {
-  let map = input.reduce( function (acc, elm) { acc[elm] = true; return acc; }, Array.apply( null, new Array(2021)).map(() => false) );
-  for (var i in input) if (input[i] != 1010 && map[2020 - input[i]]) return [input[i], 2020-input[i], input[i] * (2020-input[i])]; 
-  return [ 1010, 1010, 1010*1010 ];
-}
-
-const goB = (input) => {
-  input = input.sort( (a,b) => a-b );
-  for (let i=0; i<input.length; i++) 
-    for (let j=i+1; j<input.length && input[i] + input[j] < 2020; j++) 
-      for (let k=j+1; k<input.length && input[i] + input[j] + input[k] <= 2020; k++) 
-        if (input[i] + input[j] + input[k] == 2020) return [input[i], input[j], input[k], input[i] * input[j] * input[k]];
+const nSumProduct = function(n: number, target: number, input: Array<number>): number {
+  for (let index: Array<number> = [ 0 ]; true; index[index.length - 1]++) {
+    if (index[index.length - 1] == input.length) index.pop();
+    else if (index.length < n) index.push(index[index.length - 1]);
+    else if ( target == index.reduce( (acc, i) => acc + input[i], 0 )) return index.reduce( (acc, i) => acc * input[i], 1 );
+  }
 }
 
 console.time("Time")
-const resultA = goA(input)
-const resultB = goB(input)
+console.log("Solution to part 1:", nSumProduct(2, 2020, input))
+console.log("Solution to part 2:", nSumProduct(3, 2020, input))
 console.timeEnd("Time")
-
-console.log("Solution to part 1:", resultA)
-console.log("Solution to part 2:", resultB)
