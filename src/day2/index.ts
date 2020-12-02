371f84dc4cf8b1da42d2e07256d530f0b136f016
Sérgio Carvalho <sergio.carvalho@sooma.com>
2020-12-02 10:15:57 +0000

    day2: Remove need for @ts-ignore

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,13 +1,12 @@
 import { test, readInput } from "../utils/index"
 interface InputRow { min: number; max: number; password: string; letter: string }
-// @ts-ignore
-const input:Array<InputRow> = readInput().split("\n").map( (s:string) => {
+const input:Array<InputRow> = readInput().split("\n").filter( (s) => s.length > 0 ).map( (s:string):InputRow => {
   let parser = new RegExp('^(?<min>[0-9]+)-(?<max>[0-9]+) (?<letter>.): (?<password>.+)$');
   let result = s.match(parser);
-  return result != null ? { min: result['groups'].min, max: result['groups'].max, letter: result['groups'].letter, password: result['groups'].password } : false;
-}).filter( (x) => x ) || [];
+  return { min: +result['groups'].min, max: +result['groups'].max, letter: result['groups'].letter, password: result['groups'].password };
+});
 
-const isPasswordValid = function(password) {
+const isPasswordValid = function(password:InputRow) {
   let repeats = password.password.split('').filter( (char) => char == password.letter).length;
   return password.min <= repeats && repeats <= password.max;
 }
