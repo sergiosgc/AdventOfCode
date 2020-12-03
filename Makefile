8b732a27040942b36478fe202715e16de0888157
Sérgio Carvalho <sergio.carvalho@sooma.com>
2020-12-03 08:22:41 +0000

    Day 3

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
-all: day2
+all: day3
 
 day1:
 	npx ts-node src/day1/index.ts
 day2:
 	npx ts-node src/day2/index.ts
+day3:
+	npx ts-node src/day3/index.ts
--- src/utils/readInput.ts
+++ src/utils/readInput.ts
@@ -1,11 +1,11 @@
 import { readFileSync } from "fs"
 import * as getCallerFile from "get-caller-file"
 
-export const readInput = () => {
+export const readInput = (test=false) => {
   const file = getCallerFile()
     .split("/")
     .slice(0, -1)
-    .concat("input.txt")
+    .concat(test ? "test.input.txt" : "input.txt")
     .join("/")
 
   return readFileSync(file).toString()
