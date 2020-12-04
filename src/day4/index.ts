b46cfc94fd91657b1bc7e1ea23aba2b4e5108bd0
Sérgio Carvalho <sergio.carvalho@sooma.com>
2020-12-04 11:22:08 +0000

    day4: Use only regexes for validation

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -1,43 +1,22 @@
 import { test, readInput } from "../utils/index"
-const input:Array<Map<string, string>> = readInput().trim().split("\n\n").map( (s) => s.split("\n").map( (s) => s.split(" ") ).reduce( (acc, a) => acc.concat(a), [] ).reduce( function(acc,s) {
+const input:Array<Map<string, string>> = readInput().trim().split("\n\n").map( (s) => s.trim().split("\n").map( (s) => s.split(" ") ).reduce( (acc, a) => acc.concat(a), [] ).reduce( function(acc,s) {
   let [k,v] = s.split(":", 2);
   acc.set(k,v);
   return acc;
 }, new Map() )
 );
-const isPassportValid = function(p: Map<string, string>):boolean {
-  let regex = new Map([
-    [ "hgt", /^(?<value>[0-9]+)(?<unit>(?:in|cm))$/],
+const fieldValidationRegex = new Map([
+    [ "iyr", /^201\d|2020$/ ],
+    [ "eyr", /^202\d|2030$/ ],
+    [ "byr", /^19[2-9]\d|200[0-2]$/ ],
+    [ "hgt", /^((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in)$/ ],
     [ "hcl", /^#(?:[0-9a-f]{6})$/ ],
     [ "ecl", /^(?:amb|blu|brn|gry|grn|hzl|oth)$/],
     [ "pid", /^(?:[0-9]{9})$/ ],
-  ])
-  for (let field of p.keys()) {
-    switch (field) {
-      case 'byr':
-        if (+p.get(field) < 1920 || 2002 < +p.get(field)) return false;
-        break;
-      case 'iyr':
-        if (+p.get(field) < 2010 || 2020 < +p.get(field)) return false;
-        break;
-      case 'eyr':
-        if (+p.get(field) < 2020 || 2030 < +p.get(field)) return false;
-        break;
-      case "hgt":
-      case "hcl":
-      case "ecl":
-      case "pid":
-        if (!regex.get(field).test(p.get(field))) return false;
-        if (field != "hgt") break;
-        let match = p.get(field).match(regex.get(field));
-        let [ unit, value ] = [ match['groups']['unit'], +match['groups']['value'] ];
-        if ( unit == 'cm' && value < 150 || unit == 'cm' && 193 < value || unit == 'in' && value < 59 || unit == 'in' && 76 < value) return false;
-        break;
-    }
-  }
-  return true;
-}
+]);
+const isPassportValid = (p: Map<string, string>):boolean => Array.from(p.keys()).reduce( (acc, field) => acc && (!fieldValidationRegex.has(field) || fieldValidationRegex.get(field).test(p.get(field)) ), true );
+
 console.time("Time")
 console.log("Solution to part 1: ", input.filter( (m) => m.size == 8 || (m.size == 7 && !m.has("cid")) ).length);
-console.log("Solution to part 2: ", input.filter( (m) => m.size == 8 || (m.size == 7 && !m.has("cid")) ).filter( (p) => isPassportValid(p) ).length );
+console.log("Solution to part 2: ", input.filter( (m) => m.size == 8 || (m.size == 7 && !m.has("cid")) ).filter( isPassportValid ).length );
 console.timeEnd("Time")
