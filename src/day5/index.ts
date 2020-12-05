2005e1b887f527f4344a1e2d2ffb8fdea1e4531c
Sérgio Carvalho <sergio.carvalho@sooma.com>
2020-12-05 11:23:34 +0000

    day5: Refactor emptyIsolatedSeats()

--- src/day5/index.ts
+++ src/day5/index.ts
@@ -1,10 +1,8 @@
 import { test, readInput } from "../utils/index"
 const charBitmap = new Map([ ["F", 0 ], ["B", 1 ], [ "L", 0 ], [ "R", 1 ] ]);
-const input:Set<number> = new Set(readInput().trim().split("\n").map( (s) => s.trim().split("").map( (c) => charBitmap.get(c) ).join("") ).map( (s) => parseInt(s, 2) ));
-const emptyIsolatedSeats = function* (source:Set<number>) { 
-  for (let i=1; i<1023; i++) if (source.has(i-1) && !source.has(i) && source.has(i+1)) yield i;
-}
+const input:Array<number> = readInput().trim().split("\n").map( (s) => s.trim().split("").map( (c) => charBitmap.get(c) ).join("") ).map( (s) => parseInt(s, 2) ).sort( (a,b) => a-b );
+const emptyIsolatedSeats = (source:Array<number>) => Array.from(source.keys()).filter( (k) => k - 1 in source).map( (k) => source[k] - source[k-1] == 2 ? source[k]-1 : null ).filter( (seat) => seat != null );
 console.time("Time")
 console.log("Solution to part 1: ", Math.max(...input));
-console.log("Solution to part 2: ", Array.from(emptyIsolatedSeats(input)).filter( (id) => id > 7 && id < 1016 ).pop() );
+console.log("Solution to part 2: ", emptyIsolatedSeats(input).filter( (id) => id > 7 && id < 1016 ).pop() );
 console.timeEnd("Time")
