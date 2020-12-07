59d3ad71ac2adf9683a99760b4431bbc389c94d0
Sérgio Carvalho <sergio.carvalho@sooma.com>
2020-12-07 12:36:49 +0000

    day7: Rewrite DirectedGraph::addEdge idiomatically

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -5,10 +5,8 @@ class DirectedGraph {
 
     public constructor(init?: Partial<DirectedGraph>) { Object.assign(this, init); }
     addEdge(from:string, to:string, weight: number):DirectedGraph {
-        if (!this.directEdges.has(from)) this.directEdges.set(from, new Map<string,number>());
-        if (!this.reverseEdges.has(to)) this.reverseEdges.set(to, new Map<string,number>());
-        this.directEdges.get(from).set(to,weight);
-        this.reverseEdges.get(to).set(from,weight);
+        (this.directEdges.get(from) ?? this.directEdges.set(from, new Map<string,number>()).get(from)).set(to,weight);
+        (this.reverseEdges.get(to) ?? this.reverseEdges.set(to, new Map<string,number>()).get(to)).set(from,weight);
         return this;
     }
     neighbours = (node:string):Iterable<string> => this.directEdges.get(node)?.keys() ?? []
@@ -28,14 +26,14 @@ class DirectedGraph {
 }
 const input = readInput().trim().split("\n")
     .map( (l) => l['match'](/^(?<color>\w+ \w+) bags contain(?<contained>.*)\.$/)['groups'] )
-    .map( (m) => { return {
+    .map( (m) => ({
         contained: m.contained.split(',').map( (edge) => edge.match(/^ ?(?:(?:no other)|(?:(?<count>\d+)) (?<color>\w+ \w+)) bags?/) ).
                 filter( (edge) => edge['groups'].color ).map( (edge) => ({
                     from: m.color,
                     to: edge['groups'].color,
                     count: +edge['groups'].count
                 }) )
-    }})
+    }))
     .reduce( (graph, node) => node.contained.reduce( (graph, edge) => graph.addEdge( edge.from, edge.to, edge.count), graph), new DirectedGraph());
 const containedBags = (graph:DirectedGraph, by:string):number => 1 + Array.from(graph.neighbours(by)).map( (bag) => input.directEdges.get(by).get(bag) * containedBags(graph, bag) ).reduce( (acc, n) => acc + n, 0 )
 console.log("Part 1:", input.reversed().floodFill("shiny gold").size);
