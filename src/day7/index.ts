4795a915f497eb264911df51eec85628cde7ef8b
Sérgio Carvalho <sergio.carvalho@sooma.com>
2020-12-07 12:41:40 +0000

    day7: Refactor input parsing

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -26,15 +26,16 @@ class DirectedGraph {
 }
 const input = readInput().trim().split("\n")
     .map( (l) => l['match'](/^(?<color>\w+ \w+) bags contain(?<contained>.*)\.$/)['groups'] )
-    .map( (m) => ({
-        contained: m.contained.split(',').map( (edge) => edge.match(/^ ?(?:(?:no other)|(?:(?<count>\d+)) (?<color>\w+ \w+)) bags?/) ).
-                filter( (edge) => edge['groups'].color ).map( (edge) => ({
-                    from: m.color,
-                    to: edge['groups'].color,
-                    count: +edge['groups'].count
-                }) )
-    }))
-    .reduce( (graph, node) => node.contained.reduce( (graph, edge) => graph.addEdge( edge.from, edge.to, edge.count), graph), new DirectedGraph());
+    .map( (m) => m.contained.split(',')
+        .map( (edge) => edge.match(/^ ?(?:(?:no other)|(?:(?<count>\d+)) (?<color>\w+ \w+)) bags?/) )
+        .filter( (edge) => edge['groups'].color ).map( (edge) => ({
+            from: m.color,
+            to: edge['groups'].color,
+            count: +edge['groups'].count
+        }) )
+    )
+    .reduce( (acc, arr) => acc.concat(arr))
+    .reduce( (graph, edge) => graph.addEdge( edge.from, edge.to, edge.count), new DirectedGraph());
 const containedBags = (graph:DirectedGraph, by:string):number => 1 + Array.from(graph.neighbours(by)).map( (bag) => input.directEdges.get(by).get(bag) * containedBags(graph, bag) ).reduce( (acc, n) => acc + n, 0 )
 console.log("Part 1:", input.reversed().floodFill("shiny gold").size);
 console.log("Part 2:", containedBags(input, "shiny gold")-1);
