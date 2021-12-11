c07215cbdfccc49f958229ece6c14a104d772f25
Sérgio Carvalho <sergio.carvalho@sooma.com>
2021-12-11 21:09:51 +0000

    Cleanup code for day 11

--- src/day11/one.js
+++ src/day11/one.js
@@ -1,13 +1,15 @@
 process.stdin.setEncoding('utf-8');
 coalesce = (v,onnull) => v == null ? onnull : v;
-input = () => new Promise( (resolve, reject) => process.stdin.on('readable', async () => resolve(coalesce(process.stdin.read(), "").split("\n").filter( s => s != "" ).map( s => s.split("").map( c => c - '0' )))));
+incr = (x, y) => ++board[y][x];
+input = () => new Promise( (resolve, reject) => 
+    process.stdin.on('readable', async () => 
+        resolve(
+            coalesce(process.stdin.read(), "")
+                .split("\n").filter( s => s != "" )
+                .map( s => s.split("").map( c => c - '0' )))));
 
-var flashCount = 0;
+let flashCount = 0;
 let board = [];
-function incr(x, y) {
-    board[y][x] += 1;
-    return board[y][x];
-}
 function game() {
     board = board.map( row => row.map( cell => cell + 1 ));
     let flashes = board.map( (row, y) => row.map( (cell, x) => cell > 9 ? [y,x] : null ))
@@ -22,11 +24,9 @@ function game() {
     }
     board = board.map( row => row.map( cell => cell > 9 ? 0 : cell ));
 }
-
 async function main() {
     board = await input();
     for (let i = 0; i<100; i++) game();
     console.log(flashCount);
 }
-
 main();
