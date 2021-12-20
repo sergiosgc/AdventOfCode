4bf722907385804e9afacde704f69ca334c0c1c4
Sérgio Carvalho <sergio.carvalho@sooma.com>
2021-12-20 23:49:55 +0000

    Day 19: Beacon Scanner (non-working solution)

--- src/day18/two.ts
+++ src/day18/two.ts
@@ -55,48 +55,7 @@ function pairsFrom(arr:string[]):[Tree,Tree][] {
     return result;
 }
 
-
 async function main() {
     console.log(pairsFrom(await input()).map( (pair:[Tree,Tree]) => pair[0].add(pair[1]).magnitude() ).reduce( (a,b) => Math.max(a,b) ));
-    /*
-    let trees = pairsFrom((await input()).map( 
-        (s:string) => {
-        let result = new Tree(null);
-        let stack:Node[] = [];
-        for (let i=0; i<s.length; i++) {
-            switch (s[i]) {
-                case '[':
-                case ',':
-                    break;
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                    stack.push( new LeafNode(null, parseInt(s[i])));
-                    break;
-                case ']':
-                    let right = stack.pop();
-                    if (typeof(right) == "undefined") throw new Error("Empty stack");
-                    let left = stack.pop();
-                    if (typeof(left) == "undefined") throw new Error("Empty stack");
-                    let pair = new PairNode(null, left, right);
-                    left.parentNode = pair;
-                    right.parentNode = pair;
-                    stack.push(pair);
-            }
-        }
-        if (stack.length != 1) throw new Error("Unbalanced parens");
-        stack[0].parentNode = result;
-        result.topNode = stack[0];
-        return result;
-    }))
-    console.log(trees);
-    */
 }
 main();
