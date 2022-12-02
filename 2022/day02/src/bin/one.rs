2ff261d78d80c4140bd87fa3779d8ad4b1a96fbf
Sérgio Carvalho <sergio.carvalho@sooma.com>
2022-12-02 11:33:08 +0000

    Day 2: Rock Paper Scissors

--- 2022/day02/src/bin/one.rs
+++ 2022/day02/src/bin/one.rs
@@ -1,6 +1,47 @@
 use std::io::BufRead;
+#[derive(Debug, PartialEq, Clone)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors
+}
+#[derive(Debug)]
+struct Round {
+    mine: Move,
+    opponent: Move
+}
+impl Round {
+    fn from_string(s: String) -> Round {
+        let split = s.split(" ").collect::<Vec<&str>>();
+        Round { 
+            mine: match split[1] {
+                "X" => Move::Rock,
+                "Y" => Move::Paper,
+                "Z" => Move::Scissors,
+                _ => panic!("Unpexpected")
+            },
+            opponent: match split[0] {
+                "A" => Move::Rock,
+                "B" => Move::Paper,
+                "C" => Move::Scissors,
+                _ => panic!("Unpexpected")
+            },
+        }
+    }
+    fn score(self) -> i64 {
+        (match ( self.mine.clone(), self.opponent.clone() ) {
+            (Move::Rock, Move::Scissors) | (Move::Scissors, Move::Paper) | (Move::Paper, Move::Rock) => 6,
+            _ => if self.mine == self.opponent { 3 } else { 0 }
+        }) + (match self.mine {
+            Move::Rock => 1,
+            Move::Paper => 2,
+            Move::Scissors => 3
+        })
+    }
+}
+
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input: Vec<String> = std::io::BufReader::new(std::io::stdin()).lines().filter_map(std::io::Result::ok).collect();
-    println!("{:#?}", input);
+    println!("{:#?}", input.into_iter().map(|line| Round::from_string(line) ).map(|mve| mve.score()).sum::<i64>());
     Ok(())
 }
--- 2022/day02/src/bin/two.rs
+++ 2022/day02/src/bin/two.rs
@@ -1,6 +1,56 @@
 use std::io::BufRead;
+#[derive(Debug, PartialEq, Clone)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors
+}
+#[derive(Debug)]
+struct Round {
+    mine: Move,
+    opponent: Move
+}
+impl Round {
+    fn from_string(s: String) -> Round {
+        let split = s.split(" ").collect::<Vec<&str>>();
+        let opponent =match split[0] {
+            "A" => Move::Rock,
+            "B" => Move::Paper,
+            "C" => Move::Scissors,
+            _ => panic!("Unpexpected")
+        };
+        Round { 
+            mine: match split[1] {
+                "X" => match opponent {
+                    Move::Rock => Move::Scissors,
+                    Move::Paper => Move::Rock,
+                    Move::Scissors => Move::Paper,
+                },
+                "Y" => opponent.clone(),
+                "Z" => match opponent {
+                    Move::Rock => Move::Paper,
+                    Move::Paper => Move::Scissors,
+                    Move::Scissors => Move::Rock,
+                },
+                _ => panic!("Unpexpected")
+            },
+            opponent: opponent,
+        }
+    }
+    fn score(self) -> i64 {
+        (match ( self.mine.clone(), self.opponent.clone() ) {
+            (Move::Rock, Move::Scissors) | (Move::Scissors, Move::Paper) | (Move::Paper, Move::Rock) => 6,
+            _ => if self.mine == self.opponent { 3 } else { 0 }
+        }) + (match self.mine {
+            Move::Rock => 1,
+            Move::Paper => 2,
+            Move::Scissors => 3
+        })
+    }
+}
+
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input: Vec<String> = std::io::BufReader::new(std::io::stdin()).lines().filter_map(std::io::Result::ok).collect();
-    println!("{:#?}", input);
+    println!("{:#?}", input.into_iter().map(|line| Round::from_string(line) ).map(|mve| mve.score()).sum::<i64>());
     Ok(())
 }
