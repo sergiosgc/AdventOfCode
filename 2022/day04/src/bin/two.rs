16b6b2ac80fa014784a7512d7ed516bfab3d1f07
Sérgio Carvalho <sergio.carvalho@sooma.com>
2022-12-04 17:00:00 +0000

    Day 4: Camp Cleanup

--- 2022/day04/src/bin/two.rs
+++ 2022/day04/src/bin/two.rs
@@ -1,6 +1,17 @@
 use std::io::BufRead;
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input: Vec<String> = std::io::BufReader::new(std::io::stdin()).lines().filter_map(std::io::Result::ok).collect();
-    println!("{:#?}", input);
+    println!("{:#?}", std::io::BufReader::new(std::io::stdin()).lines().filter_map(std::io::Result::ok)
+        .map(|line| line
+            .replace("-", ",")
+            .split(",")
+            .map( |v| v.parse::<i64>().unwrap() )
+            .collect::<Vec<i64>>()
+        ).map(|line| [
+            (line[0], line[1]), 
+            (line[2], line[3]), 
+            (line[0].max(line[2]), line[1].min(line[3]))
+        ]).filter(|ranges| ranges[2].0 <= ranges[2].1)
+        .count()
+    );
     Ok(())
 }
