c24bb191c448797533a1d9ecd589215914efa0cd
Sérgio Carvalho <sergio.carvalho@sooma.com>
2022-12-05 16:46:51 +0000

    Day 5: Supply Stacks

--- 2022/day05/src/bin/two.rs
+++ 2022/day05/src/bin/two.rs
@@ -1,6 +1,64 @@
 use std::io::BufRead;
+use itertools::Itertools;
+use regex::Regex;
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input: Vec<String> = std::io::BufReader::new(std::io::stdin()).lines().filter_map(std::io::Result::ok).collect();
-    println!("{:#?}", input);
+    let (stack_input, move_input) = std::io::BufReader::new(std::io::stdin())
+        .lines()
+        .filter_map(std::io::Result::ok).collect::<Vec<String>>()
+        .split(|line| line.len() == 0)
+        .map(|slice| slice.into_iter().map(|s| s.clone() ).collect::<Vec<String>>())
+        .next_tuple::<(Vec<String>, Vec<String>)>()
+        .unwrap();
+    let stacks = stack_input
+        .into_iter()
+        .filter(|line| line.find('[').is_some())
+        .map(|line| line
+            .replace("    ", "[_] ")
+            .replace("[", "")
+            .replace("]", "")
+            .replace(" ", "")
+            .chars()
+            .collect::<Vec<char>>())
+        .fold(Vec::<Vec<char>>::new(), |mut acc, line| {
+            for (i,ch) in line.into_iter().enumerate() {
+                while i+1 > acc.len() { acc.push(Vec::<char>::new()); }
+                acc[i].push(ch);
+            }
+            acc
+
+        })
+        .into_iter()
+        .map(|stack| stack.into_iter().filter(|ch| ch != &'_').collect::<Vec<char>>())
+        .map(|mut stack| { stack.reverse(); stack })
+        .collect::<Vec<Vec<char>>>()
+        ;
+    let moves = move_input
+        .into_iter()
+        .map(|line| {
+            let captures = Regex::new(r"[^0-9]*(\d+)[^0-9]*(\d+)[^0-9]*(\d+)")
+                .unwrap()
+                .captures(&line)
+                .unwrap();
+            (
+                captures.get(1).unwrap().as_str().parse::<i64>().unwrap(), 
+                captures.get(2).unwrap().as_str().parse::<usize>().unwrap() - 1,
+                captures.get(3).unwrap().as_str().parse::<usize>().unwrap() - 1
+            )
+        })
+        .collect::<Vec<(i64, usize, usize)>>();
+    println!("{:#?}", moves
+        .into_iter()
+        .fold(stacks, |mut stack, (count, from, to)| {
+            let stack_from = stack[from].clone();
+            let (left, right) = stack_from.split_at(stack_from.len() - count as usize);
+            right.into_iter().for_each( |r| stack[to].push(r.clone()) );
+            stack[from] = left.to_vec();
+            stack
+        })
+        .into_iter()
+        .map(|mut stack| stack.pop().ok_or(' ').unwrap() )
+        .collect::<String>()
+        .replace(" ", "")
+    );
     Ok(())
 }
