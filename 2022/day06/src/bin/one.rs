e9b02ae5fb4d4bf6a2866fcafface6689fde3159
Sérgio Carvalho <sergio.carvalho@sooma.com>
2022-12-06 11:48:27 +0000

    Day 6: Tuning Trouble

--- 2022/day06/src/bin/one.rs
+++ 2022/day06/src/bin/one.rs
@@ -1,6 +1,18 @@
 use std::io::BufRead;
+use regex::Regex;
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input: Vec<String> = std::io::BufReader::new(std::io::stdin()).lines().filter_map(std::io::Result::ok).collect();
-    println!("{:#?}", input);
+    println!("{:#?}", input
+        .into_iter()
+        .map(|line| Regex::new(r"(.)(?!\1)(.)(?!\1|\2)(.)(?!\1|\2|\3)(.)(?!\1|\2|\3|\4)")
+            .unwrap()
+            .captures(&line)
+            .unwrap()
+            .get(1)
+            .unwrap()
+            .end()
+        )
+        .collect::<Vec<usize>>()
+    );
     Ok(())
 }
