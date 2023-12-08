4ac44be1ac36a5c9054cc00ba0e826c5bb6be3c3
Sérgio Carvalho <sergio.carvalho@sooma.com>
2023-12-08 10:52:00 +0000

    Day 8: Haunted Wasteland

--- 2023/day08/src/bin/one.rs
+++ 2023/day08/src/bin/one.rs
@@ -1,6 +1,30 @@
-use std::io::BufRead;
+use std::{io::BufRead, collections::HashMap};
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input: Vec<String> = std::io::BufReader::new(std::io::stdin()).lines().filter_map(std::io::Result::ok).collect();
-    println!("{:#?}", input);
+    let mut input = std::io::BufReader::new(std::io::stdin())
+        .lines()
+        .filter_map(std::io::Result::ok);
+    let mut instructions = input.next().unwrap().chars().collect::<Vec<char>>().into_iter().cycle().enumerate();
+    _ = input.next();
+    let network = input
+    .fold(HashMap::<String, (String, String)>::new(), |mut acc, line| {
+        let (key, rest) = line
+        .split_once("=")
+        .unwrap();
+        let (left, right) = rest.split_once(",").unwrap();
+        acc.insert(key.replace(" ", "").to_string(), (left.replace(" ", "").replace("(", "").to_string(), right.replace(" ", "").replace(")", "").to_string()));
+        acc
+    });
+    let mut current = "AAA".to_string();
+    let count = {
+        while current != "ZZZ".to_string() {
+            current = match instructions.next().unwrap().1 {
+                'L' => network.get(&current).unwrap().0.clone(),
+                _ => network.get(&current).unwrap().1.clone() 
+            };
+        }
+        instructions.next().unwrap().0
+    };
+
+    println!("{:#?}", count);
     Ok(())
 }
--- 2023/day08/src/bin/two.rs
+++ 2023/day08/src/bin/two.rs
@@ -1,6 +1,41 @@
-use std::io::BufRead;
+use std::{io::BufRead, collections::HashMap};
+pub fn solve_for(start: String, network: &HashMap<String, (String, String)>, instructions: &Vec<char>) -> i64 {
+    let mut instructions_iter = instructions.into_iter().cycle().enumerate();
+    let mut current = start.clone();
+    while current.clone().split_off(2) != "Z".to_string() {
+        current = match instructions_iter.next().unwrap().1 {
+            'L' => network.get(&current).unwrap().0.clone(),
+            _ => network.get(&current).unwrap().1.clone() 
+        };
+    }
+    instructions_iter.next().unwrap().0 as i64
+}
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input: Vec<String> = std::io::BufReader::new(std::io::stdin()).lines().filter_map(std::io::Result::ok).collect();
-    println!("{:#?}", input);
+    let mut input = std::io::BufReader::new(std::io::stdin())
+        .lines()
+        .filter_map(std::io::Result::ok);
+    let instructions = input.next().unwrap().chars().collect::<Vec<char>>();
+    _ = input.next();
+    let network = input
+    .fold(HashMap::<String, (String, String)>::new(), |mut acc, line| {
+        let (key, rest) = line
+        .split_once("=")
+        .unwrap();
+        let (left, right) = rest.split_once(",").unwrap();
+        acc.insert(
+            key.replace(" ", "").to_string(), 
+            (left.replace(" ", "").replace("(", "").to_string(),
+               right.replace(" ", "").replace(")", "").to_string())
+        );
+        acc
+    });
+
+    println!("{:#?}", network
+    .clone()
+    .into_iter()
+    .map(|v| v.0)
+    .filter(|node| node.clone().split_off(2) == "A".to_string())
+    .map(|start| solve_for(start, &network, &instructions))
+    .fold(1_i64, num::integer::lcm));
     Ok(())
 }
