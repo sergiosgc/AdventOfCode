0d4b563310b96af521600fcb776d059da69bff4c
Sérgio Carvalho <sergio.carvalho@sooma.com>
2023-12-09 10:37:29 +0000

    Day 9: Mirage Maintenance

--- 2023/day09/src/bin/one.rs
+++ 2023/day09/src/bin/one.rs
@@ -1,6 +1,32 @@
 use std::io::BufRead;
+pub fn derive(seq: &Vec<i64>) -> Vec<i64> {
+    let mut result: Vec<i64> = Vec::new();
+    let mut iter = seq.iter();
+    let mut last = *iter.next().unwrap();
+    for current in iter {
+        result.push(*current - last);
+        last = *current;
+    }
+    result
+}
+pub fn predict_next(seq: &Vec<i64>) -> i64 {
+    if seq.iter().find(|v| **v != 0).is_some() {
+        let last = seq.len() - 1;
+        seq[last] + predict_next(&derive(seq))
+    } else {
+        0
+    }
+}
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input: Vec<String> = std::io::BufReader::new(std::io::stdin()).lines().filter_map(std::io::Result::ok).collect();
-    println!("{:#?}", input);
+    let input: Vec<Vec<i64>> = std::io::BufReader::new(std::io::stdin())
+    .lines()
+    .filter_map(std::io::Result::ok)
+    .map(|line| line
+        .split_whitespace()
+        .flat_map(str::parse::<i64>)
+        .collect::<Vec<i64>>()
+    )
+    .collect();
+    println!("{:#?}", input.iter().map(|seq| predict_next(seq)).sum::<i64>());
     Ok(())
 }
