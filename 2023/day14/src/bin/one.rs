34dfb5178e18895cc35b61301e25a7d3c35c383a
Sérgio Carvalho <sergio.carvalho@sooma.com>
2023-12-14 16:59:04 +0000

    Day 14: Parabolic Reflector Dish

--- 2023/day14/src/bin/one.rs
+++ 2023/day14/src/bin/one.rs
@@ -1,6 +1,111 @@
-use std::io::BufRead;
+use std::{io::BufRead, collections::HashMap};
+use aoc::coord::Coord;
+use itertools::Itertools;
+
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash, Ord, PartialOrd)]
+enum Rock {
+    #[default]
+    Square,
+    Round
+}
+#[derive(Clone, Default, PartialEq, Eq)]
+struct Platform {
+    pub size: Coord,
+    pub rocks: HashMap<Coord, Rock>
+}
+impl std::hash::Hash for Platform {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.size.hash(state);
+        let mut rocks = self.rocks.iter().collect::<Vec<(&Coord, &Rock)>>();
+        rocks.sort_by(|a,b| a.0.cmp(b.0));
+        rocks.hash(state);
+    }
+}
+impl std::fmt::Debug for Platform {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        (0..self.size.y+1).cartesian_product(0..self.size.x+1).for_each(|(y, x)| {
+            write!(f, "{}", match self.rocks.get(& Coord { x, y }) {
+                Some(Rock::Square) => "#",
+                Some(Rock::Round) => "O",
+                None => "."
+            }).unwrap();
+            if x == self.size.x { write!(f, "\n").unwrap(); }
+        });
+        Ok(())
+    }
+}
+impl Platform {
+    pub fn tilt(&mut self) {
+        for x in 0..self.size.x+1 {
+            let mut squares = self.rocks.iter()
+            .filter(|(coord, rock)| coord.x == x && **rock == Rock::Square)
+            .map(|c| c.0.y)
+            .collect::<Vec<i64>>();
+            squares.push(-1);
+            squares.push(self.size.y+1);
+            squares.sort();
+            let new_rocks = squares
+            .windows(2)
+            .map(|v| (v[0], v[1]))
+            .flat_map(|(min, max)|
+                (min+1..min+1+self.rocks.iter()
+                .filter(|(coord, rock)| coord.x == x && coord.y > min && coord.y < max && **rock == Rock::Round)
+                .count() as i64).collect::<Vec<i64>>()
+            )
+            .map(|y| (Coord { x, y }, Rock::Round) )
+            .collect::<Vec<(Coord, Rock)>>();
+            let old_rocks = self.rocks.iter()
+                .filter(|(coord, rock)| coord.x == x && **rock == Rock::Round)
+                .map(|c| c.0.clone())
+                .collect::<Vec<Coord>>();
+            for coord in old_rocks { self.rocks.remove(&coord); }
+            for rock in new_rocks { self.rocks.insert( rock.0, rock.1 ); }
+        }
+    }
+    pub fn load(self) -> i64 {
+        self.rocks.iter()
+        .filter(|(_, rock)| **rock == Rock::Round)
+        .map(|(coord, _)| self.size.y - coord.y + 1)
+        .sum::<i64>()
+    }
+}
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input: Vec<String> = std::io::BufReader::new(std::io::stdin()).lines().filter_map(std::io::Result::ok).collect();
-    println!("{:#?}", input);
+    let mut input = {
+        let input = std::io::BufReader::new(std::io::stdin())
+        .lines()
+        .filter_map(std::io::Result::ok)
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line
+            .chars()
+            .enumerate()
+            .map(|(x, ch)| {
+                (
+                    Coord { x: x.try_into().unwrap(), y: y.try_into().unwrap() },
+                    match ch {
+                        '#' => Some(Rock::Square),
+                        'O' => Some(Rock::Round),
+                        _ => None
+                    }
+                )
+            })
+            .collect::<Vec<(Coord, Option<Rock>)>>()
+        })
+        .collect::<Vec<(Coord, Option<Rock>)>>();
+        Platform {
+            size: input
+                  .iter()
+                  .map(|v| v.0)
+                  .reduce(|a, b| Coord::bounding_max(&a, &b))
+                  .unwrap(),
+            rocks: input
+                   .iter()
+                   .filter(|(_, rock)| rock.is_some())
+                   .map(|(coord, rock)| (coord.clone(), rock.unwrap().clone()))
+                   .collect()
+        }
+    };
+    input.tilt();
+    println!("{:#?}", input.load());
     Ok(())
 }
