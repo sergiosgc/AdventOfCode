bd22a90761bc1c2ecd7531ab7c4fc30c55b5da84
Sérgio Carvalho <sergio.carvalho@sooma.com>
2024-12-02 12:58:34 +0000

    Day 2: Red-Nosed Reports

--- 2024/day02/two.nim
+++ 2024/day02/two.nim
@@ -20,14 +20,25 @@ var input =
                   .filter( part => part != "")
                   .map(parseInt)
     )
+var deltas: seq[seq[seq[int]]]
+for row in input:
+    var candidates: seq[seq[int]]
+    candidates.add(seqDeltas(row))
+    for i in 0..row.len-1:
+        var changedRow = row
+        changedRow.delete(i)
+        candidates.add(seqDeltas(changedRow))
+    deltas.add(candidates)
 echo(
-    input
-    .map( seqDeltas )
-    .map( deltaRow => (
-        deltaRow
+    deltas
+    .map( puzzle => # seq[seq[int]]
+     puzzle.map( candidateDeltas => # seq[int]
+        candidateDeltas
         .map( delta => (delta, abs(delta) > 0 and abs(delta) < 4))
         .foldl( (b[0], a[1] and b[1] and a[0]/abs(a[0]) == b[0]/abs(b[0]) ) )
-    ))
-    .filter( a => a[1] )
+     )
+     .foldl( a or b[1], false)
+    )
+    .filter( a => a )
     .len()
 )
