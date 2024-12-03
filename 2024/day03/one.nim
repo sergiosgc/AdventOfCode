9db7bbffbac72f77de595ec96334519789015952
Sérgio Carvalho <sergio.carvalho@sooma.com>
2024-12-03 11:29:07 +0000

    Day 3: Mull It Over

--- 2024/day03/one.nim
+++ 2024/day03/one.nim
@@ -2,12 +2,26 @@ import sugar
 import sequtils
 import strutils
 import system
-import std/algorithm
+import std/re
 
 var input =
     stdin
     .lines
     .toSeq
+var regex = re"mul\((\d+),(\d+)\)"
 echo(
     input
+    .map( line => line
+        .findAll(regex)
+        .toSeq() 
+        .map( s => (
+            var matches: array[0..1, string]
+            discard s.match(regex, matches)
+            matches
+            .map(parseInt)
+        ))
+        .map( val => val[0] * val[1])
+        .foldl( a + b )
+    )
+    .foldl( a + b )
 )
---
+++ 2024/day03/two.nim
@@ -0,0 +1,47 @@
+import sugar
+import sequtils
+import strutils
+import system
+import std/re
+
+var input =
+    stdin
+    .lines
+    .toSeq
+var regex = re"mul\((\d+),(\d+)\)|do\(\)|don't\(\)"
+echo(
+    input
+    .map( line => line
+        .findAll(regex)
+        .toSeq() 
+        .map( s => (
+            case s:
+                of "do()":
+                    (0, 1)
+                of "don't()":
+                    (0, -1)
+                else:
+                    var matches: array[0..1, string]
+                    discard s.match(regex, matches)
+                    (matches
+                     .map(parseInt)
+                     .foldl( a * b )
+                     , 0)
+        ))
+    )
+    .foldl( a.concat(b) )
+    .foldl( 
+        case b[1]:
+            of 1:
+                (a[0], 1)
+            of -1:
+                (a[0], 0)
+            else:
+                case a[1]:
+                    of 0:
+                        a
+                    else:
+                        (a[0] + b[0], 1)
+        , (0,1)
+    )[0]
+)
